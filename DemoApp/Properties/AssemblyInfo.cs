543fb003cc960dad111c6e9d36ffbad7ae7eac63
Mikhail Filippov <mikhail@filippov.me>
2015-03-12 13:59:36 +0300

    Init project.

---
+++ DemoApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoApp")]
+[assembly: AssemblyDescription("Demo WinForms aplication")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikhail Filippov")]
+[assembly: AssemblyProduct("DemoApp")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a14f75a-b1f9-4869-a074-e65fe773bdd1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
